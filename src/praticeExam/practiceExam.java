11735b170bf4e445da6abed2e6cb7e2b55ae255b
John Mark Bagamaspad <12203450@students.koi.edu.au>
2023-10-02 23:43:33 +1100

    First Practice

---
+++ src/praticeExam/practiceExam.java
@@ -0,0 +1,53 @@
+package praticeExam;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class practiceExam {
+    public static void main(String[] args) {
+
+        /*int[] table = new int[10];
+        for (int x = 1; x < 10; x++) {
+            table[x] = 99;
+        }
+        try {
+            File file = new File("MyFile.txt");
+            Scanner inputFile = new Scanner(file);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        }
+        int a, b, c; //Three integers
+        a = 3;
+        b = 4;
+        c = a + b;
+        System.out.println("The value of c is" + c);
+
+        //The variable names references an integer array with 20 elements. Write a for loop that prints each element of the array.
+        int[] variable_name = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+        // Loop through the array and print each element
+        for (int i = 0; i < variable_name.length; i++) {
+            System.out.println(variable_name[i]);
+        }
+
+
+        //Write an if-else statement that assigns 100 to x when y is equal to 10. Otherwise it should assign 1 to x.
+        int y = 10;
+        int x;
+
+        if (y == 10) {
+            x = 100;
+        } else {
+            x = 1;
+        }
+
+        System.out.println("The value of x is: " + x);*/
+        //What happens when you compare two Strings with the == operator in java
+       // In Java, when you use the == operator to compare two strings, you are comparing the references to the string objects, not the actual contents of the strings. This means that == checks whether the two string references point to the same memory location, rather than whether the strings have the same characters.
+
+        //How do you add items to an ArrayList object?
+        //In Java, you can add items to an ArrayList object using the .add() method provided by the ArrayList class. Here's how you can do it:
+    }
+}
+
